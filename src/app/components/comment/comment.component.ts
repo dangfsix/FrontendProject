d4017d61340c17de53d1b398f491bfa198a986bd
Nguyễn Văn Quyết <ngvanquyet2000@gmail.com>
2021-09-17 16:16:23 +0700

    Xây dựng CommentComponent và CommentService
    - Thêm chức năng Thêm comment
    - Thêm chức năng Sửa comment
    - Thêm chức năng Xóa comment
    
    Co-Authored-By: Dang <60851390+pthdang@users.noreply.github.com>

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Comment, User } from 'src/app/app.interfaces';
-import { CommentService } from 'src/app/services/comment.service';
-import { UserService } from 'src/app/services/user.service';
+import {Component, Input, OnInit} from '@angular/core';
+import {Comment, User} from 'src/app/app.interfaces';
+import {CommentService} from 'src/app/services/comment.service';
+import {UserService} from 'src/app/services/user.service';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-comment',
@@ -12,11 +13,15 @@ export class CommentComponent implements OnInit {
   @Input() productId: number = 0;
   public comments: Comment[] = [];
   public userId: number = 0;
+  public currentEditCommentId: number = 0;
+  public isEdit: boolean = false;
 
   constructor(
     private commentService: CommentService,
-    private userService: UserService
-  ) { }
+    private userService: UserService,
+    private route: ActivatedRoute,
+  ) {
+  }
 
   ngOnInit(): void {
     this.comments = this.commentService.getListByProductId(this.productId);
@@ -29,16 +34,71 @@ export class CommentComponent implements OnInit {
   }
 
   public create(): void {
-    console.log('Create');
+    let commentId = Math.round(Math.random() * 10000000000);
+    let userId = this.userService.getCurrentUser().id;
+    const productId = +this.route.snapshot.params['id'];
+    let ratingScore = document.getElementsByTagName("select")[0].value;
+    let commentText = document.getElementsByTagName("textarea")[0].value;
+    let date = this.commentService.getDateCreateComment();
+
+    let btnCreate: any = document.getElementById('create-comment');
+
+    if (ratingScore.length == 1 && commentText != '') {
+      let cmt: Comment = {
+        commentId: commentId,
+        userId: userId,
+        productId: productId,
+        commentText: commentText,
+        ratingScore: Number(ratingScore),
+        date: date,
+      }
+
+      this.commentService.addComment(cmt);
+      this.ngOnInit();
+      btnCreate.setAttribute('type', 'reset');
+      alert('Thêm thành công');
+
+    } else {
+      if (ratingScore.length > 1)
+        alert('Vui lòng cho sao');
+      else if (commentText == '')
+        alert('Vui lòng thêm nội dung đánh giá');
+      btnCreate.setAttribute('type', '');
+    }
   }
 
   public edit(event: any): void {
-    console.log('Edit');
-    console.log(event.target.dataset.commentId);
+    this.currentEditCommentId = event.target.dataset.commentId;
+  }
+
+  public submitEdit() {
+    let selectEdit: any = document.getElementById('edit-ratingScore');
+    let textareaEdit: any = document.getElementById('edit-commentText');
+
+    let ratingScore = selectEdit.value;
+    let commentText = textareaEdit.value;
+
+    if (ratingScore.length == 1 && commentText != '') {
+      this.commentService.editComment(this.currentEditCommentId, ratingScore, commentText);
+      this.ngOnInit();
+      alert('Sửa thành công');
+
+    } else if (ratingScore.length > 1)
+      alert('Vui lòng cho sao');
+    else if (commentText == '')
+      alert('Vui lòng thêm nội dung đánh giá');
+  }
+
+  public cancelEdit() {
+    this.currentEditCommentId = -1;
   }
 
   public delete(event: any): void {
-    console.log('Delete');
-    console.log(event.target.dataset.commentId);
+    let choose = confirm('Xác nhận xoá');
+    if (choose) {
+      let commentId = event.target.dataset.commentId;
+      this.commentService.deleteComment(commentId);
+      this.ngOnInit();
+    }
   }
 }
--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,25 +1,92 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as data from '../../assets/data/comments.json';
-import { Comment } from '../app.interfaces';
+import {Comment} from '../app.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  private commentsJSON: Comment[] = [];
+  private commentsAdded: Comment[] = [];
+  private commentsDeleted: Comment[] = [];
   private comments: Comment[] = [];
 
   constructor() {
     this.init();
   }
 
-  private init(): void {
-    let commentsJSON: Comment[] = (data as any).default;
-    localStorage.setItem('comments', JSON.stringify(commentsJSON));
-    this.comments = JSON.parse(localStorage.getItem('comments') || '[]');
+  public init(): void {
+    this.commentsJSON = (data as any).default;
+    localStorage.setItem('commentsJSON', JSON.stringify(this.commentsJSON));
+
+    this.refreshComments();
   }
 
   public getList(): Comment[] {
     return this.comments;
+  }
+
+  public refreshComments() {
+    this.comments = JSON.parse(localStorage.getItem('commentsJSON') || '[]');
+
+    this.commentsAdded = JSON.parse(localStorage.getItem('added') || '[]');
+    if (this.commentsAdded.length != 0)
+      this.comments = this.comments.concat(this.commentsAdded);
+
+    this.commentsDeleted = JSON.parse(localStorage.getItem('deleted') || '[]');
+    if (this.commentsDeleted.length != 0) {
+      for (let i = 0; i < this.commentsDeleted.length; i++) {
+        for (let j = 0; j < this.comments.length; j++) {
+          if (this.comments[j].commentId == this.commentsDeleted[i].commentId)
+            this.comments.splice(j, 1);
+        }
+      }
+    }
+  }
+
+  public addComment(cmt: Comment) {
+    this.commentsAdded.push(cmt);
+    localStorage.setItem('added', JSON.stringify(this.commentsAdded));
+    this.refreshComments();
+  }
+
+  public editComment(commentId: number, ratingScore: number, commentText: string) {
+    let commentNeedToEdit = this.getItemByCommentId(commentId);
+
+    let newCommentId = Math.round(Math.random() * 10000000000);
+    let date = this.getDateCreateComment();
+
+    this.commentsDeleted.push(commentNeedToEdit);
+    localStorage.setItem('deleted', JSON.stringify(this.commentsDeleted));
+
+    // Tạo mới 1 Comment để thêm vào CommentAdded
+    // Thực chất là tạo 1 commentId mới, và giữ nguyên productId và userId
+    let cmt = {
+      commentId: newCommentId,
+      productId: commentNeedToEdit.productId,
+      userId: commentNeedToEdit.userId,
+      ratingScore: ratingScore,
+      commentText: commentText,
+      date: date,
+    }
+
+    this.addComment(cmt);
+  }
+
+  public deleteComment(commentId: number) {
+    this.commentsDeleted.push(this.getItemByCommentId(commentId));
+
+    localStorage.setItem('deleted', JSON.stringify(this.commentsDeleted));
+    this.refreshComments();
+  }
+
+  public getDateCreateComment() {
+    let date;
+    let day = new Date().getDate();
+    let month = new Date().getMonth() + 1;
+    let year = new Date().getFullYear();
+
+    return date = year + '-' + month + '-' + day;
   }
 
   public getListByProductId(productId: number): Comment[] {
@@ -27,7 +94,7 @@ export class CommentService {
   }
 
   public getItemByCommentId(commentId: number): Comment {
-    return this.comments.find(comment => comment.commentId === commentId)!;
+    return this.comments.find(comment => comment.commentId == commentId)!;
   }
 
   public sortByDate(comments: Comment[]) {
