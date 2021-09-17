4cd982c3830fc3e192bab85dd96f1e38cdb25a93
Dang <60851390+pthdang@users.noreply.github.com>
2021-09-17 22:05:34 +0700

    Update search.component.ts

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -4,7 +4,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/app.interfaces';
 import { FilterPipe } from 'src/app/pipes/filter.pipe';
-import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -20,7 +19,6 @@ export class SearchComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private categoryService: CategoryService,
     private productService: ProductService,
     private filterPipe: FilterPipe
   ) { }
@@ -35,14 +33,5 @@ export class SearchComponent implements OnInit {
 
   public onChangePage(pageOfItems: any[]): void {
     this.pageOfItems = pageOfItems;
-  }
-
-  // Các hàm filter không sử dụng được
-  public filterSidebar(event: any) {
-    this.products = this.categoryService.filterSidebar(event, this.products, this.route);
-  }
-
-  public filterSelect(event: any) {
-    this.products = this.categoryService.filterSelect(event, this.products, this.route);
   }
 }
